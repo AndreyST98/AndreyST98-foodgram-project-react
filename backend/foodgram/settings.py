a6b7fce37e0626275022627da717e7a456b1f352
AndreyST98 <andre.stepanyuk@yandex.ru>
2022-11-09 23:20:34 +0700

    63

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -49,10 +49,11 @@ ROOT_URLCONF = 'foodgram.urls'
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': ['rest_framework.permissions.AllowAny', ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 3,
+    'PAGE_SIZE': 6,
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
     ],
     'DEFAULT_FILTER_BACKENDS': [
         'django_filters.rest_framework.DjangoFilterBackend',
